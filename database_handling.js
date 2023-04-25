a7b0d63e0daec2f52539f5cf6fe673a824ed7b33
mariusz5581 <mariusz5581@gmail.com>
2023-04-25 21:29:59 +0300

    1

--- database_handling.js
+++ database_handling.js
@@ -1,41 +1,107 @@
 //this is database_handling.js
 const sqlite3 = require('sqlite3').verbose();
 let db = [];
+initializeUserDatabase();
 
-db[0] = new sqlite3.Database('users.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the users.db database.');
-}); 
+function initializeUserDatabase() {
 
-
-function createNewUserDatabase(username, userId) {
-  const dbName = `${username}_${userId}.db`;
-  const userDb = new sqlite3.Database(dbName, (err) => {
+  db[0] = new sqlite3.Database('users.db', (err) => {
     if (err) {
       console.error(err.message);
-    } else {
-      console.log(`Created new database for user ${username} with ID ${userId}`);
     }
+    console.log('Connected to the users.db database.');
   });
 
-  return userDb;
-}
-
-db[0].serialize(() => {
-  db[0].run(`CREATE TABLE IF NOT EXISTS users (
+  db[0].serialize(() => {
+    db[0].run(`CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE NOT NULL,
             password TEXT NOT NULL
           )`, (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('Users table created or already exists.');
+      }
+    });
+  });
+
+  db[0].serialize(() => {
+    db[0].all(`SELECT * FROM users`, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(rows);
+      }
+    });
+  });
+
+  db[0].all('SELECT username FROM users', (err, rows) => {
     if (err) {
       console.error(err.message);
     } else {
-      console.log('Users table created or already exists.');
+      // Iterate through users and create a new _transactions.db file for each user
+      rows.forEach((row) => {
+        let dbName = `${row.username}.db`;
+        let dbTransactions = new sqlite3.Database(dbName, (err) => {
+          if (err) {
+            console.error(err.message);
+          }
+          console.log(`Connected to the ${dbName} database.`);
+  
+          // Create transactions table for the user
+          dbTransactions.run(
+            `CREATE TABLE IF NOT EXISTS transactions (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              date TEXT,
+              category TEXT,
+              title TEXT,
+              ammount_base REAL,
+              ammount_percent_tax REAL,
+              ammount_tax REAL,
+              from_account TEXT,
+              to_account TEXT,
+              note TEXT
+            )`,
+            (err) => {
+              if (err) {
+                console.error(err.message);
+              } else {
+                console.log(`Transactions table created for user ${row.username}.`);
+              }
+            }
+          );
+        });
+  
+        // Close the connection to the _transactions.db file
+        /*dbTransactions.close((err) => {
+          if (err) {
+            console.error(err.message);
+          }
+          console.log(`Connection to the ${dbName} database closed.`);
+        });*/
+        dbTransactions.serialize(() => {
+          db[0].all(`SELECT * FROM transactions`, (err, rows) => {
+            if (err) {
+              console.error(err.message);
+            } else {
+              console.log(rows);
+            }
+          });
+        });
+        db.add(dbTransactions);
+      });
     }
   });
-});
+  
+  // Close the connection to the users.db file
+  /*dbUsers.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connection to the users.db database closed.');
+  });*/
+}
 
 function addNewTransactionsTable(username, userId){
     db[userId].serialize(() => {
@@ -83,19 +149,10 @@ function addNewInvoicesTable(username, userId){
   });
 }
 
-db[0].serialize(() => {
-    db[0].all(`SELECT * FROM users`, (err, rows) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log(rows);
-      }
-    });
-  });
+
   
   module.exports = {
     db: db,
-    createNewUserDatabase: createNewUserDatabase,
     addNewTransactionsTable: addNewTransactionsTable,
     addNewInvoicesTable: addNewInvoicesTable,
   };
